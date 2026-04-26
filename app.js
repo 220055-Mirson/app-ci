4e7f69a7ef52aa517914ced6cbd8229e2ae182f2
Mirsosn Belito <mirsonbelitom@gmail.com>
2026-04-26 23:55:16 +0200

    Adicionando pagina web para CD!

--- app.js
+++
@@ -1,1 +0,0 @@
-console.log("Aplicacao CI/CD funcionando");
---
+++ server.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    let filePath = '.' + req.url;
+    if (filePath === './') filePath = './index.html';
+    
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            res.writeHead(404);
+            res.end('Arquivo não encontrado');
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(content);
+        }
+    });
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+    console.log("Aplicação CI/CD funcionando!");
+});
